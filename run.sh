c19d0402ebf11b2feda62162505a95e8c45f875b
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2011-06-18 14:53:48 +0800

    Fix a bug in run.sh
    Only run the strat=5.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ make
 
 for ((i = 1;i<=20;i=$i+1)) 
 do
-	for ((j = 4; j<=9; j=$j+1))
+	for ((j = 5; j<=5; j=$j+1))
 	do
 		#	sed "s/^functionToRun [0-9]*$/functionToRun $i/" configure.ini > temp
 		#	cat temp > configure.ini
