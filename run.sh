cb4d25536ee222ec75745c687016be57daf5815d
Wenxiang Chen--Laptop <chenwx.ustc@gmail.com>
2011-06-24 19:44:49 +0800

    Add the support mutiple runs for various dimension

--- run.sh
+++ run.sh
@@ -5,13 +5,16 @@ for ((i = 1;i<=20;i=$i+1))
 do
 	for ((j = 5; j<=5; j=$j+1))
 	do
-		#	sed "s/^functionToRun [0-9]*$/functionToRun $i/" configure.ini > temp
-		#	cat temp > configure.ini
-		echo funcID $i, learnStrat $j
-		#	cat configure.ini | grep '^functionToRun [0-9]*$'
-		# ./submit.sh $i
-		qsub ccvil.pbs -v jobID=$i,learnStrat=$j
-		sleep 2
+		for k in 50 100 200 500 1000 2000
+		do
+			#	sed "s/^functionToRun [0-9]*$/functionToRun $i/" configure.ini > temp
+			#	cat temp > configure.ini
+			echo funcID $i, learnStrat $j, dimension $k
+			#	cat configure.ini | grep '^functionToRun [0-9]*$'
+			# ./submit.sh $i
+			qsub ccvil.pbs -v fun=$i,strat=$j,dim=$k
+			sleep 2
+		done
 	done
 done
 
